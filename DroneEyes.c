7c44036008b9b05fd93c8829ed3b92eb942d2369
Andy <andy@StuffAndyMakes.com>
2015-07-08 09:09:51 -0700

    Moved ALL ON mode to last before OFF

--- DroneEyes.c
+++ DroneEyes.c
@@ -28,17 +28,17 @@ int ledNum = 0;
 #define SCAN_LEFT -1
 
 #define STATE_OFF 0
-#define STATE_ALL 1
-#define STATE_BOUNCE_SLOW 2
-#define STATE_BOUNCE_MED 3
-#define STATE_BOUNCE_FAST 4
-#define STATE_SCAN_LEFT_SLOW 5
-#define STATE_SCAN_LEFT_FAST 6
-#define STATE_SCAN_RIGHT_SLOW 7
-#define STATE_SCAN_RIGHT_FAST 8
-#define STATE_BLINK_SLOW 9
-#define STATE_BLINK_MED 10
-#define STATE_BLINK_FAST 11
+#define STATE_BOUNCE_SLOW 1
+#define STATE_BOUNCE_MED 2
+#define STATE_BOUNCE_FAST 3
+#define STATE_SCAN_LEFT_SLOW 4
+#define STATE_SCAN_LEFT_FAST 5
+#define STATE_SCAN_RIGHT_SLOW 6
+#define STATE_SCAN_RIGHT_FAST 7
+#define STATE_BLINK_SLOW 8
+#define STATE_BLINK_MED 9
+#define STATE_BLINK_FAST 10
+#define STATE_ALL 11
 #define STATE_MAX 12
 
 uint8_t state = STATE_ALL; // start in all on state
@@ -113,7 +113,7 @@ int main(void) {
                 state++;
                 if (state == STATE_MAX) {
                     state = STATE_OFF;
-                    allOff();
+					allOff();
                 }
 	            while ((PINB & (1 << PB4)) == 0); // wait for release
             }
@@ -125,10 +125,6 @@ int main(void) {
 
         // main case for state
         switch (state) {
-                
-            case STATE_ALL:
-				allOn();
-                break;
                 
             case STATE_BOUNCE_SLOW:
 				bounce();
@@ -181,6 +177,10 @@ int main(void) {
 				blink(5000);
                 break;
                 
+            case STATE_ALL:
+				allOn();
+				break;
+            
         }
         
     }
